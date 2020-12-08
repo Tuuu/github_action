ffb972aea04303dc7821589de667f24e341b306e
tutengfei <tutengfei1994@gmail.com>
2020-12-08 18:08:20 +0800

    test cache fourth

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.{actionSubModules('test cache third')}
+          Edit <code>src/App.js</code> and save to reload.{actionSubModules('test cache fourth')}
         </p>
         <a
           className="App-link"
