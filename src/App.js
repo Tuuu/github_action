db65c2b07b62f49343c6995a00fc02c2478ba465
tutengfei <tutengfei1994@gmail.com>
2020-12-08 18:02:20 +0800

    test cache again

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.{actionSubModules('test cache')}
+          Edit <code>src/App.js</code> and save to reload.{actionSubModules('test cache again')}
         </p>
         <a
           className="App-link"
